3b57e125e0c649e476d77b51d8a3da65f724c301
mahelie <mahelie.bergeron@gmail.com>
2025-04-29 09:23:40 -0400

    Add bootstrap

---
+++ src/app/composant/footer.js
@@ -0,0 +1,13 @@
+export default function footer() {
+    return <>
+
+            <footer>
+            
+            </footer>
+            
+            {/*Mettre mes scripts*/}
+            <script></script>
+        
+    </>
+
+}
