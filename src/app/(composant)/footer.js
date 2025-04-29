4416b44e9d8848dacfe22bbe7936697486370c68
GobEth2333500 <143568944+GobEth2333500@users.noreply.github.com>
2025-04-29 11:41:46 -0400

    popper

--- src/app/(composant)/footer.js
+++ src/app/(composant)/footer.js
@@ -15,6 +15,7 @@ export default function footer() {
         <script src="https://unpkg.com/localbase/dist/localbase.dev.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossOrigin="anonymous"></script>
         <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.min.js" integrity="sha256-AlTido85uXPlSyyaZNsjJXeCs07eSv3r43kyCVc8ChI=" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </>
 
 }
