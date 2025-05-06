71bd693d2fd5e103b4e6c4f6c39974819c6196a6
mahelie <mahelie.bergeron@gmail.com>
2025-05-06 14:33:46 -0400

    Commencer le html de la page inscription

--- src/app/inscription/page.js
+++ src/app/inscription/page.js
@@ -1,1 +1,51 @@
 
+export default function Inscription() {
+    //doit faire un fetch post qunad on clique sur submit
+
+
+
+    
+    
+    
+    return <>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-4 mx-auto">
+                        <h1> Inscription </h1>
+                        <form id = "formInscription">
+                            <div className="mb-3">
+                                <label htmlFor="Nom" className="form-label">Nom</label>
+                                <input type="text" className="form-control" id="Nom" />
+                            </div>
+                            <div> 
+                                <label htmlFor="Prenom" className="form-label">Prenom</label>
+                                <input type="text" className="form-control" id="Prenom" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="Pseudo" className="form-label">Pseudo</label>
+                                <input type="text" className="form-control" id="Pseudo" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="Email1" className="form-label">Email address</label>
+                                <input type="email" className="form-control" id="Email1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="Password1" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="Password" />
+                            </div>
+                            <div className="mb-3 form-check">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+  
+    
+    </>
+    
+}
