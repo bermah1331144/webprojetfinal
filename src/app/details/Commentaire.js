e7aacbe41348c9a40297022b3550cbba24d90f02
mahelie <mahelie.bergeron@gmail.com>
2025-05-06 09:32:15 -0400

    Commencer mes fetch, il ont des problemes, je vais les regles 1 a la fois

--- src/app/details/Commentaire.js
+++ src/app/details/Commentaire.js
@@ -1,21 +1,26 @@
 //afficher 1 seul commentaire
 "use client";
 
-import Commentaire from "./Commentaire";
+
+import Commentaire from "../../../bd.json";
 export default function commentaire({commentaire}) {
     //il va recevoir toute les infos d'un seul commentaire  
     
     //reste a teste, pour afficher un seul commentaire
 
+    console.log(commentaire);
+
     //------------------ATTENTION!!!! DOIS CHANGER LES ATTIBUTS DANS ID="COMMENTAIRE" -> <p> & <p> ------------------
     return <>
+    
         <div id="boiteCommentaire"  className="d-flex- flex-row-reverse">
             <div id="userIcon">
                 <i className="bi bi-person-square"></i>
             </div>
             <div id="commentaire">
-                <p>Nom du commentaire</p>
-                <p>Cette object est merveilleux</p>
+                <p>{Commentaire.titre}</p>
+                <p>{Commentaire.date}</p>
+                <p>{Commentaire.contenu}</p>
             </div>
         </div>
     </>;
