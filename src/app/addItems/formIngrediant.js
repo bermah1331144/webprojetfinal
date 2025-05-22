046f593d54914c4f83d8aa3c0d55dd4e3eac5cac
mahelie <mahelie.bergeron@gmail.com>
2025-05-22 11:10:53 -0400

    Commencer a faire mes fetchs pour le form

--- src/app/addItems/formIngrediant.js
+++ src/app/addItems/formIngrediant.js
@@ -1,9 +1,61 @@
 "use client";
 
+import { useEffect, useState, useContext } from "react";
+
 export default function FormIngrediant() {
+    //gestion de chamgement dans le form
+    const [ingrediants, setIngrediants] = useState({
+        ingrediant1: "",
+        ingrediant2: "",
+        ingrediant3: ""
+    });
+
+    //gestion de chamgement dans le form
+    const handleCharge = (e) => {
+
+        setIngrediants({
+            ...ingrediants,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    //Prend les infos donner par le user et le push sur l BD
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!ingrediants.ingrediant1 || !ingrediants.ingrediant2 || !ingrediants.ingrediant3) {
+            alert("Veuillez remplir tous les champs");
+            return ;
+        }
+
+        try {
+            const response = fetch(`http://localhost:3001/ingrediants`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(ingrediants),
+            });
+            if (response.ok) {
+                console.log("Ingrédiant ajouté avec succès");
+                setIngrediants({
+                    ingrediant1: "",
+                    ingrediant2: "",
+                    ingrediant3: ""
+                });
+            } else {
+                console.error("Erreur lors de l'ajout de l'ingrediant");
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    
+    
     return <>
                 <div>
-                    <form id="recette-igendiants">
+                    <div id="recette-igendiants">
                         <div className="mb-3">
                             <label htmlFor="ingredient1" className="form-label">Ingrédient 1:</label>
                             <input
@@ -11,7 +63,9 @@ export default function FormIngrediant() {
                                 className="form-control"
                                 id="ingredient1"
                                 name="ingredient1"
-                                
+                                value={ingrediants.ingrediant1}
+                                onChange={handleCharge}
+                                required
                             />
                         </div>
 
@@ -36,9 +90,10 @@ export default function FormIngrediant() {
                             />
                         </div>
                             <div className="d-flex justify-content-end">
-                                <button className="btn custom-btn mt-3" type="submit">Ajouter</button>
+                                <button onClick={handleSubmit} className="btn custom-btn mt-3">Ajouter</button>
                             </div>
-                    </form>
+                        
+                    </div>
                 </div>
     
     
--- src/app/addItems/page.js
+++ src/app/addItems/page.js
@@ -1,23 +1,73 @@
 "use client";
 
 import FormIngrediant from "./formIngrediant";
-import { useState } from "react";
 import './style.sass';
 import '../(style)/style.sass';
+import { useEffect, useState, useContext } from "react";
 
 export default function Page() {
-    
+
+    //Permet de prendre les info ecrit par usager
+    const [items, setItems] = useState({
+        image: "",
+        name: "",
+        description: "",
+        rarity: 0,
+        ingrediants: [],
+        prixVentre: 0,
+        prixAchat: 0
+    });
+
+    //gestion de chamgement dans le form
+    const handleCharge = (e) => {
+        setItems({
+            ...items,
+            [e.target.name]: e.target.value
+        });
+    }
+
     //dois faire un fetch push pour ajouter un item dans BD
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        if(!items.image || !items.name || !items.description || !items.rarity || !items.ingrediants || !items.prixVentre || !items.prixAchat) {
+            alert("Veuillez remplir tous les champs");
+            return ;
+        }
 
+        try {
+            const response = await fetch(`http://localhost:3001/items`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(items),
+            });
+            if (response.ok) {
+                console.log("Item ajouté avec succès");
+                setItems({
+                    image: "",
+                    name: "",
+                    description: "",
+                    rarity: 0,
+                    ingrediants: [],
+                    prixVentre: 0,
+                    prixAchat: 0
+                });
+            } else {
+                console.error("Erreur lors de l'ajout de l'item");
+            }
+        } catch (err) {
+            console.error("Erreur lors de l'ajout de l'item", err);
+        }
+    }
     //doit prendre les info ecrit par usager et les add a la bd
-
     //doit monter un form pour ajouter un item
-    
     return (
         <div id="imgFondForm"className="container-fluid h-80">
             <div className="row ">
                 <div id="addItem"className="col-4 mx-auto my-5">
-                    <form className="justify-content-center mx-auto">
+                    <form onSubmit={handleSubmit}  className="justify-content-center mx-auto">
                             <h1>Ajout d'un nouvel item</h1>
                         <div className="mb-3">
                             <label htmlFor="image" className="form-label">Image:</label>
@@ -27,18 +77,10 @@ export default function Page() {
                                 id="image"
                                 name="image"
                                 accept="image/*"
+                                value={items.image}
+                                onChange={handleCharge}
                                 required
                             />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="description" className="form-label">Description:</label>
-                            <textarea
-                                className="form-control"
-                                id="description"
-                                name="description"
-                                required
-                            
-                            ></textarea>
                         </div>
 
                         <div className="mb-3">
@@ -48,9 +90,25 @@ export default function Page() {
                                 className="form-control"
                                 id="name" 
                                 name="name"
+                                value={items.name}
+                                onChange={handleCharge}
                                 required
                             />
                         </div>
+                        <div className="mb-3">
+                            <label htmlFor="description" className="form-label">Description:</label>
+                            <textarea
+                                className="form-control"
+                                id="description"
+                                name="description"
+                                value={items.description}
+                                onChange={handleCharge}
+                                required
+                            
+                            ></textarea>
+                        </div>
+
+{/*------------------------------------- Comment faire pour aller chercher le chois de utilsateur a partir de la liste destroulante ? --------------------------------- */}
 
                         <div className="mb-3">
                             <label htmlFor="rarity" className="form-label">Rareté:</label>
@@ -60,7 +118,8 @@ export default function Page() {
                                 <option value="Épique">Épique</option>                            <option value="Légendaire">Légendaire</option>
                             </select>
                         </div>
-
+{/*-------------------------------------------Probleme peut pas mettre un form dans un form le deuxieme form doit etre un div,
+----------------------------------------------Mais la pas capable decrire dans les case allouer ---------------------------------------*/}
                         <div className="mb-3">
                             <p>DEBUT FORM INGREDIANT</p>
                             <FormIngrediant/>
@@ -74,6 +133,8 @@ export default function Page() {
                                 className="form-control"
                                 id="prixVente"
                                 name="prixVente"
+                                value={items.prixVente}
+                                onChange={handleCharge}
                                 required
                             />
                         </div>
@@ -85,6 +146,8 @@ export default function Page() {
                                 className="form-control"
                                 id="prixAchat"
                                 name="prixAchat" 
+                                value={items.prixAchat}
+                                onChange={handleCharge}
                                 required
                             />
                         </div>
